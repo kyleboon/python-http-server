c658920deac59283cf8e0fd14aa09ca3fc65c7bd
Kyle Boon <kyle.boon@target.com>
2024-01-28 10:18:43 -0600

    Server shuts down after test completion

--- server_test.py
+++ server_test.py
@@ -1,17 +1,18 @@
+import threading
 import requests
 from threading import Thread
 from server import HttpConnectionListener 
 
 def create_server():
-  server = HttpConnectionListener()
+  server = HttpConnectionListener("127.0.0.1", 8080)
   server.listen()
   thread = Thread(target = server.run_event_loop, args=())
   thread.start()
   return (server, thread)
 
 def test_httpget():
-  server, _ = create_server()
+  server, thread = create_server()
   response = requests.get("http://127.0.0.1:8080")
   assert response.status_code == 200
-  server.stop
-  
+  server.stop()
+  thread.join()  
